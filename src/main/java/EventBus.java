1ea88525f1340b5f7747dc4fa051b201a1f51caf
macmini <aakashthesid@gmail.com>
2023-02-22 19:14:34 +0530

    Adding the logic for read/ write

--- src/main/java/EventBus.java
+++ src/main/java/EventBus.java
@@ -1,55 +1,80 @@
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class EventBus {
     private Map<String, List<Event>> topics;
-    private Map<String, Set<String>> pullSubscribers;
-    private Map<String, Set<String>> pushSubscribers;
-    KeyedExecutor executor;
+    // S1 -> map (T1 -> offset) (T2 -> offset2)
+    // S2 -> map (T2 -> offsetX)
+    private Map<String, Map<String, Long>> subscriberMetaData;
+    private WriteExecutor writeExecutor;
+    private ReadExecutor readExecutor;
 
     public EventBus() {
         topics = new ConcurrentHashMap<>();
-        pullSubscribers = new ConcurrentHashMap<>();
-        executor = new KeyedExecutor(10);
+        subscriberMetaData = new ConcurrentHashMap<>();
+        writeExecutor = new WriteExecutor(10);
+        readExecutor = new ReadExecutor(10);
     }
 
+    // Publish will run asynchronously
     public void publish(String topic, Event event) {
-        executor.submit(topic, () -> topics.get(topic).add(event));
-        topics.get(topic).add(event);
+        writeExecutor.submitTask(topic, () -> topics.get(topic).add(event));
     }
 
-    public void subscribePull(String topic, String subscriber) {
-        pullSubscribers.get(topic).add(subscriber);
+    // Subscriber S1 registers to start reading from topic T1 with a offset
+    // Remember where you last left after reading from the topic
+    public void subscribe(String topic, String subscriberId) {
+        Map<String, Long> topicOffset = subscriberMetaData.get(subscriberId);
+        if (topicOffset == null) {
+            topicOffset.put(topic, 1L);
+        } else {
+            // Already subscribed
+        }
     }
 
-    public void subscribePush(String topic, String subscriber) {
-        pushSubscribers.get(topic).add(subscriber);
+    // Subscriber S1, needs to follow order in which the events are published
+    // While reading only one thread should read what is the content for subscriber + topic pair
+    public void read(String topic, String subscriberId) {
+        // TODO:
+        subscriberMetaData.get(subscriberId).get(topic);
     }
-
 }
 
-class KeyedExecutor {
+class WriteExecutor {
     private Executor[] executors;
 
-    public KeyedExecutor(int threads) {
-        for (int count = 0; count< threads; count++) {
+    public WriteExecutor(int threads) {
+        for (int count = 0; count < threads; count++) {
             executors[count] = Executors.newSingleThreadExecutor();
         }
     }
 
+    // To make sure that the order is maintained, take a hash of the topic and modulo by Total threads
+    // so all the requests on 1 topic will always be taken care by one thread.
+    // The task is just to add elements to the topic
+    public <T> CompletableFuture<T> submitTask(String topic, Supplier<T> task) {
+        return CompletableFuture.supplyAsync(task, executors[topic.hashCode() % executors.length]);
+    }
+}
 
-    public CompletionStage<Void> submit(String id, Runnable task) {
-        return CompletableFuture.runAsync(task, executors[id.hashCode() % executors.length]);
+class ReadExecutor {
+    private Executor[] executors;
+
+    public ReadExecutor(int thread) {
+        for (int count = 0; count < thread; count++) {
+            executors[count] = Executors.newSingleThreadExecutor();
+        }
     }
 
-
-    public <T> CompletionStage<T> get(String id, Supplier<T> task) {
-        return CompletableFuture.supplyAsync(task, executors[id.hashCode() % executors.length]);
+    public <T> CompletableFuture<T> readEvents(String topic, String subscriberId, Map<String, List<Event>> topics) {
+        // TODO:
+        return null;
     }
 }
 
@@ -70,11 +95,11 @@ abstract class RetryAlgorithm<P, R> {
     public abstract void retry(Function<P, R> task, P param);
 }
 
-// TODO: Will do later
+// TODO: Complete later
 class ExponentialBackOff<P, R> extends RetryAlgorithm<P, R> {
 
     @Override
-    public void retry(Function<P, R> task, P  param) {
+    public void retry(Function<P, R> task, P param) {
         try {
             task.apply(param);
         } catch (Exception exception) {
